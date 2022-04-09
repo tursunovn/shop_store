58748ad918db668b24c859c3d1e02eff46374ae0
Jon <100859974+tursunovn@users.noreply.github.com>
2022-04-09 21:05:32 +0500

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', ProductList.as_view(), name='product_list'),
+    path('category/<slug:slug>/', ProductListByCategory.as_view(), name='product_list_by_category')
+]
