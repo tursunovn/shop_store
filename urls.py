d8dd0b44edf5844c70e37d9ecc374d2050813bb4
Jon <100859974+tursunovn@users.noreply.github.com>
2022-04-11 21:07:02 +0500

    Add files via upload

--- urls.py
+++ urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', ProductList.as_view(), name='product_list'),
-    path('category/<slug:slug>/', ProductListByCategory.as_view(), name='product_list_by_category')
+    path('category/<slug:slug>/', ProductListByCategory.as_view(), name='product_list_by_category'),
+    path('product/<slug:slug>/', ProductDetail.as_view(), name='product_detail')
 ]
